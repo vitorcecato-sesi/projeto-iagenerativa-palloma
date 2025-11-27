a4fa46d4b131903422280f26a8d2b71a075b5218
Vitor Cecato <vitor.cecatosesi@gmail.com>
2025-11-27 08:09:59 -0300

    adição de rotas

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,11 +2,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import './App.css'
 
+import Login from './pages/Login/Login.jsx'
+
 function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
